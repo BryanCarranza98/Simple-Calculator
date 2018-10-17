5d36ae135b6afc4570b7b9e95231f66f8e69d08a
Bryan Carranza <bryancarranza@bryans-macbook-pro.local>
2018-10-17 09:30:45 -0500

    Initial Commit

---
+++ 223 MIDTERM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  223 MIDTERM
+//
+//  Created by Bryan Carranza on 10/17/18.
+//  Copyright © 2018 Bryan Carranza. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
